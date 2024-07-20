f4a732dc8ec773a50c3395104735b03ac5175edf
kaz4mi <132118592+kaz4mi@users.noreply.github.com>
2024-07-20 11:10:52 +0500

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ let today = new Date,
 document.getElementById("age")
 	.innerHTML = new Date(years)
 	.getFullYear() - 1970
+// JavaScript
+$('#menu-opener').click(function() {
+  $('body').addClass('menu-open');
+  $('.btn-close-menu').click(function() {
+    $('body').removeClass('menu-open');
+  });
+  $('.overlay').click(function() {
+    $('body').removeClass('menu-open');
+  });
+  $('.main-navigation').prepend('');
+  $('.main-navigation ul .menu-item-has-children').app
+});
